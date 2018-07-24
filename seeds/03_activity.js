e79b4c903fd9c69d1b715e8d5422e31a45d254d1
Conor Hooley <cchooley@gmail.com>
2018-07-23 22:09:13 -0600

    Updated activity.js seed and migration files

--- seeds/03_activity.js
+++ seeds/03_activity.js
@@ -2,13 +2,22 @@ exports.seed = function (knex, Promise) {
   return knex("activity").del().then(() => {
     return knex("activity").insert([{
       id: 1,
-      activity: 'New Poll',
+      activity: 'created a poll',
       openedOn: new Date(),
       title: 'Should Vintage Theatre purchase its current building from the city of Aurora?',
       createdBy: 'Conor Hooley',
       image: 'https://tinyurl.com/ycc9cak8'
-    }]);
+    },
+    {
+      id: 2,
+      activity: 'created a profile',
+      openedOn: new Date(),
+      title: null,
+      createdBy: 'Stasia Vinnedge',
+      image: 'https://tinyurl.com/y9c9xrm4'
+    }
+  ]);
   }).then(() => {
-    return knex.raw("ALTER SEQUENCE activity_id_seq RESTART WITH 2;");
+    return knex.raw("ALTER SEQUENCE activity_id_seq RESTART WITH 3;");
   });
 };
