2554bca20eb4231080554dc50b452e79a85f072c
Conor Hooley <cchooley@gmail.com>
2018-07-25 00:41:14 -0600

    More heroku error-fixing

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.use('/users', users)
 app.use('/votes', votes)
 app.use('/auth', auth)
 app.use('/activities', activities)
-app.use('/stripe', stripe)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- queries/stripe.js
+++ queries/stripe.js
@@ -1,19 +1,19 @@
-const database = require('../database-connection')
+// const database = require('../database-connection')
 
-module.exports = {
-    list() {
-        return database('payment').select()
-    },
-    read(id) {
-        return database('payment').select().where('id', id).first()
-    },
-    create(payment) {
-        return database('payment').insert(payment).returning('*').then(record => record[0])
-    },
-    update(id, payment) {
-        return database('payment').where('id', id).update(payment, '*').then(record => record[0])
-    },
-    delete(id) {
-        return database('payment').where('id', id).del()
-    }
-}
+// module.exports = {
+//     list() {
+//         return database('payment').select()
+//     },
+//     read(id) {
+//         return database('payment').select().where('id', id).first()
+//     },
+//     create(payment) {
+//         return database('payment').insert(payment).returning('*').then(record => record[0])
+//     },
+//     update(id, payment) {
+//         return database('payment').where('id', id).update(payment, '*').then(record => record[0])
+//     },
+//     delete(id) {
+//         return database('payment').where('id', id).del()
+//     }
+// }
