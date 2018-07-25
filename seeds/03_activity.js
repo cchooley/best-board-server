0c1b405b1809c5523e31ac08850970ccd0afb2ce
Conor Hooley <cchooley@gmail.com>
2018-07-25 00:29:39 -0600

    Fixed data type for openedOn on activity migration & seeds

--- seeds/03_activity.js
+++ seeds/03_activity.js
@@ -3,7 +3,7 @@ exports.seed = function (knex, Promise) {
     return knex("activity").insert([{
       id: 1,
       activity: 'created a poll',
-      openedOn: new Date(),
+      openedOn: "07/15/2018",
       title: 'Should Vintage Theatre purchase its current building from the city of Aurora?',
       createdBy: 'Conor Hooley',
       image: 'https://tinyurl.com/ycc9cak8'
@@ -11,7 +11,7 @@ exports.seed = function (knex, Promise) {
     {
       id: 2,
       activity: 'created a profile',
-      openedOn: new Date(),
+      openedOn: "07/20/2018",
       title: null,
       createdBy: 'Stasia Vinnedge',
       image: 'https://tinyurl.com/y9c9xrm4'
@@ -19,7 +19,7 @@ exports.seed = function (knex, Promise) {
     {
       id: 3,
       activity: 'paid yearly dues',
-      openedOn: new Date(),
+      openedOn: "07/22/2018",
       title: null,
       createdBy: 'Margaret Norwood',
       image: 'https://tinyurl.com/y9yslrys'
@@ -27,7 +27,7 @@ exports.seed = function (knex, Promise) {
     {
       id: 4,
       activity: 'uploaded a new file',
-      openedOn: new Date(),
+      openedOn: "07/22/2018",
       title: null,
       createdBy: 'Bernie Cardell',
       image: 'https://tinyurl.com/y9n2pf8y'
